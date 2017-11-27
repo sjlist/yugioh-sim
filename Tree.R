aed45b4e682aa42616e082f6390b64e0b01febb5
Alton Barbehenn <abarbehenn@irobot.com>
2017-11-27 15:37:35 -0500

    Added R Project for analysis. This is just to get the ball rolling, I may end up doing these analyses in python instead (although you can run python in RStudio...)

---
+++ Tree.R
@@ -0,0 +1,5 @@
+# This script will be to try to use/visualize/analyze the game state tree
+# We'll probably limiting visualizing to one turn, becuase otherwise it will be too unweildly
+#   (maybe just color the nodes by a metric to then look at specific cases)
+#
+# This will require full annotations of the simulation (or be ported to python)
